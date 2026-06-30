a3ad0d53c792fc18089d259c5cdcd483a9bbef4c
Chrisxie12 <twumgyanchristian2@gmail.com>
2026-06-30 12:24:19 +0000

    week 4 and 4

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Core Requirement 3: Random number 1-100
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        // Core Requirement 2: Loop until guess matches
+        while (guess != magicNumber)
+        {
+            // Core Requirement 1: Ask for guess
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            // Core Requirement 1: Higher, Lower, or Correct
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
